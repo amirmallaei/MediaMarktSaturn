6e16626354f1a77a7f18eccfedd72f5d52f2de60
amirmallaei <amirmallaei@gmail.com>
2024-07-06 10:18:21 +0330

    bash edited

--- src/bashes/createDB.sh
+++ src/bashes/createDB.sh
@@ -4,6 +4,6 @@ python3 manage.py makemigrations security_service &
 
 python3 manage.py migrate &
 
-python3 manage.py test
-# python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py test &
+python3 manage.py runserver 0.0.0.0:8000
 #gunicorn config.wsgi -c ./gunicorn/gunicorn.py -b 0.0.0.0:80
